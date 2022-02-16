d2ac07f65f9ba4bf08da3b203eda46efa7d1011b
Matt Yokan <myokan@quadrex.org>
2022-02-15 18:28:46 -0600

    Annotate command handler

--- src/main/java/net/analyse/plugin/commands/AnalyseCommand.java
+++ src/main/java/net/analyse/plugin/commands/AnalyseCommand.java
@@ -6,6 +6,7 @@ import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
+import org.jetbrains.annotations.NotNull;
 
 import java.util.Arrays;
 import java.util.HashMap;
@@ -25,7 +26,7 @@ public class AnalyseCommand implements CommandExecutor {
     }
 
     @Override
-    public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
+    public boolean onCommand(@NotNull CommandSender sender, @NotNull Command command, @NotNull String label, String[] args) {
         if (args.length == 0 || !commands.containsKey(args[0].toLowerCase())) {
             sender.sendMessage(plugin.parse("&b[Analyse] &7Running on &bv" + plugin.getDescription().getVersion() + "&7."));
             return true;
