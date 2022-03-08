923bf2aff931f3537b12a6d6c832370bf608ce5a
kyle <kgrahammatzen@gmail.com>
2022-03-08 16:38:05 -0500

    Library updates/fixes
    Previously the SDK had to be installed manually in the repository to probably function.

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,4 +1,4 @@
-import com.github.jengelman.gradle.plugins.shadow.tasks.ShadowJar;
+import com.github.jengelman.gradle.plugins.shadow.tasks.ShadowJar
 
 plugins {
     java
@@ -10,16 +10,17 @@ repositories {
     maven("https://hub.spigotmc.org/nexus/content/repositories/snapshots/")
     maven("https://repo.extendedclip.com/content/repositories/placeholderapi/")
     maven("https://repo.maven.apache.org/maven2/")
+    maven("https://jitpack.io")
     mavenCentral()
 }
 
 dependencies {
     implementation("net.sf.trove4j:trove4j:3.0.3")
-    implementation("net.analyse:sdk:1.0.1")
+    implementation("com.github.track:sdk:main-SNAPSHOT")
 
     compileOnly("me.clip:placeholderapi:2.11.1")
     compileOnly("org.spigotmc:spigot-api:1.18-R0.1-SNAPSHOT")
-    compileOnly("org.jetbrains:annotations:16.0.2")
+    compileOnly("org.jetbrains:annotations:22.0.0")
 }
 
 group = "net.analyse"
@@ -47,6 +48,6 @@ tasks.register<Copy>("copyJarToServerPlugins") {
     into(layout.projectDirectory.dir("server/plugins"))
 }
 
-tasks.withType<JavaCompile>() {
+tasks.withType<JavaCompile> {
     options.encoding = "UTF-8"
 }
